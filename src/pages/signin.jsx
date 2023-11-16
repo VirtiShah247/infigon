fdb0741792b5edec41f02cef847fda3f512a7389
Virti Shah <vpshah247@gmail.com>
2023-11-16 23:38:59 +0530

    Update signin

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -68,28 +68,8 @@ const Signin = () => {
                 <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
 
                     <div className="text-white mb-3 mb-md-0">
-                        Copyright © 2020. All rights reserved.
+                        Copyright © 2023. All rights reserved.
                     </div>
-                    <div>
-
-                        <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-                            <MDBIcon fab icon='facebook-f' size="md" />
-                        </MDBBtn>
-
-                        <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-                            <MDBIcon fab icon='twitter' size="md" />
-                        </MDBBtn>
-
-                        <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-                            <MDBIcon fab icon='google' size="md" />
-                        </MDBBtn>
-
-                        <MDBBtn tag='a' color='none' className='mx-3' style={{ color: 'white' }}>
-                            <MDBIcon fab icon='linkedin-in' size="md" />
-                        </MDBBtn>
-
-                    </div>
-
                 </div>
 
             </MDBContainer>
